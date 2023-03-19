f30a9daeef44b1f4e45572d15593d9e077d7df31
avshir <av.shirinskaya@gmail.com>
2023-03-19 20:28:41 +0100

    fix: update Header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import './header.css';
 
 export default class Header extends Component<{}, {}> {
   render() {
-    return <header className='header'>My header</header>;
+    return (
+      <header className='header'>
+        <nav className='container'>
+          <ul className='nav-menu'>
+            <li>
+              <Link className='link' to='/'>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/404'>Page 404</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
   }
 }
