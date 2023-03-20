2c93851c329a6aef730c3686d83e54e497197aeb
avshir <av.shirinskaya@gmail.com>
2023-03-20 03:06:02 +0100

    feat: add Card, CardList Component, update HomePage.tsx

---
+++ src/components/cards-list/cards-list.tsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import './cards-list.scss';
+
+import Card from '../card';
+import { IProduct } from '../types';
+
+type CardListProps = {
+  products: IProduct[];
+};
+type CardListState = {};
+
+export default class CardList extends Component<CardListProps, CardListState> {
+  cards = this.props.products.slice(0, 8).map((product) => {
+    const { id, ...productProps } = product;
+
+    return (
+      <li key={id} className='cards-list__item'>
+        <Card product={product} />
+      </li>
+    );
+  });
+
+  render() {
+    return <ul className='cards-list'>{this.cards}</ul>;
+  }
+}
--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 
 import './homePage.scss';
+import CardList from '../../components/cards-list';
+import { dataGoods } from '../../data/data';
+import { IProduct } from '../../components/types';
 
-export default class HomePage extends Component<{}, {}> {
+type HomePageProps = {};
+type HomePageState = {};
+
+export default class HomePage extends Component<HomePageProps, HomePageState> {
   render() {
+    const products: IProduct[] = dataGoods.products;
+
     return (
       <div className='home-page container '>
         <h2 className='title'>HomePage</h2>
+        <CardList products={products} />
       </div>
     );
   }
