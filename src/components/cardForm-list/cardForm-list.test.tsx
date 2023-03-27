e6273734867cd164d3e82716caeffdc8e137d60a
avshir <av.shirinskaya@gmail.com>
2023-03-27 18:36:46 +0200

    feat: add tests for CardFormList component

---
+++ src/components/cardForm-list/cardForm-list.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+
+import { testCardsForm } from '../../testData/testCardsForm';
+import CardFormList from './cardForm-list';
+
+describe('test CardFormList component', () => {
+  test('render CardFormList', () => {
+    render(<CardFormList cardsForm={testCardsForm} />);
+    expect(screen.getByTestId('card-form-list')).toBeInTheDocument();
+  });
+
+  test('should be render all CardsForm in CardFormList ', () => {
+    render(<CardFormList cardsForm={testCardsForm} />);
+    expect(screen.getAllByRole('card-form').length).toBe(testCardsForm.length);
+  });
+});
---
+++ src/testData/testCardsForm.ts
@@ -0,0 +1,34 @@
+import { ICardForm } from '../components/cardForm/cardForm';
+
+export const testCardsForm: ICardForm[] = [
+  {
+    id: 101,
+    userName: 'Ivan Bobrov',
+    gender: 'male',
+    birthday: '21.02.1980',
+    country: 'Poland',
+    isConsentPersonalData: true,
+    feedbackText: 'Lorem ipsum dolor',
+    imageSrc: 'https://i.dummyjson.com/data/products/25/thumbnail.jpg',
+  },
+  {
+    id: 102,
+    userName: 'ALen Koch',
+    gender: 'female',
+    birthday: '21.02.1990',
+    country: 'German',
+    isConsentPersonalData: true,
+    feedbackText: 'Lorem ipsum dolor, sit.',
+    imageSrc: 'https://i.dummyjson.com/data/products/25/thumbnail.jpg',
+  },
+  {
+    id: 103,
+    userName: 'Anna Sh',
+    gender: 'female',
+    birthday: '21.02.2222',
+    country: 'Belarus',
+    isConsentPersonalData: true,
+    feedbackText: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    imageSrc: 'https://i.dummyjson.com/data/products/25/thumbnail.jpg',
+  },
+];
