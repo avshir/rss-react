badf817578de84514ab9014c5117d384dcbabe3f
avshir <av.shirinskaya@gmail.com>
2023-04-07 12:38:17 +0200

    fix: update test data

---
+++ src/data/dataMovie.ts
@@ -0,0 +1,14 @@
+import { IMovie } from '../components/types';
+
+export const dataMovie: IMovie = {
+  adult: false,
+  id: 550,
+  original_language: 'en',
+  original_title: 'Fight Club',
+  overview:
+    'A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground "fight clubs" forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.',
+  poster_path: '/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg',
+  release_date: '1999-10-15',
+  title: 'Fight Club',
+  vote_average: 8.431,
+};
