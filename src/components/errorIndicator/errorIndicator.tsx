9d199dd164b582af0b5da27bb040d419c8fa4f30
avshir <av.shirinskaya@gmail.com>
2023-04-07 12:33:48 +0200

    feat: add Spinner, ErrorIndicator components

---
+++ src/components/errorIndicator/errorIndicator.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './errorIndicator.scss';
+
+const ErrorIndicator = () => {
+  return (
+    <div className='error-indicator'>
+      <span className='error-indicator__title'>Ups!</span>
+      <span>something has gone wrong...</span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
