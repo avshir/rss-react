2dff315ec87a3e81b8ea2439ffcebc5716821807
avshir <av.shirinskaya@gmail.com>
2023-04-08 16:46:48 +0200

    feat: add movies-services.tsx for get data from API about movies

---
+++ src/services/movies-services.tsx
@@ -0,0 +1,20 @@
+import { IMovie } from '../components/types';
+
+const _apiBase = 'https://api.themoviedb.org/3';
+const _apiKey = '75b017a3a227731c05610048a94948e5';
+
+const getResource = async (url: string) => {
+  const res = await fetch(`${_apiBase}${url}?api_key=${_apiKey}`);
+
+  //handle all answers, except 200 ok
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}, received ${res.status}`);
+  }
+  return await res.json();
+};
+
+export const getTrendingMovies = async (time: 'week' | 'day'): Promise<IMovie[]> => {
+  const res = await getResource(`/trending/movie/${time}`);
+  const trendingMovies: IMovie[] = res.results;
+  return trendingMovies;
+};
