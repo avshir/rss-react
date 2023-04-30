bc61a148e6b94ffdbc1b966432dd65aa4b732628
avshir <av.shirinskaya@gmail.com>
2023-05-01 00:24:15 +0200

    fix: update App component to functional

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import './App.scss';
@@ -9,20 +9,20 @@ import Page404 from '../../pages/page404';
 
 import Layout from '../layout';
 
-export default class App extends Component<{}, {}> {
-  render() {
-    return (
-      <div className='App wrapper' role='app'>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<HomePage />}></Route>
-            <Route path='/about' element={<AboutPage />}></Route>
-            <Route path='/form' element={<FormPage />}></Route>
-            <Route path='/404' element={<Page404 />}></Route>
-            <Route path='*' element={<Page404 />}></Route>
-          </Route>
-        </Routes>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='App wrapper' role='app'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage />}></Route>
+          <Route path='/about' element={<AboutPage />}></Route>
+          <Route path='/form' element={<FormPage />}></Route>
+          <Route path='/404' element={<Page404 />}></Route>
+          <Route path='*' element={<Page404 />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
